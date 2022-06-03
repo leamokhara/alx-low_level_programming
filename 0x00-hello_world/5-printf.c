4d108e2b8a9e3eb7b60e85e10b0c0f47dac407f5
leamokhara <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@github.com>
2022-06-03 13:00:59 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
-/**
- * first program in c
- * main enty point
- * Return: 0 if succesful
- */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art\n");
 	return (0);
 }
