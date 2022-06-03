2c35d7421caf3ef5d42e6138dd43f50469624810
leamokhara <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@github.com>
2022-06-03 11:01:20 -0700

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 /**
- *
  * * main-Entry point
  *
  * * Return: 0 if successful
  *
- * */
+ * 
+ */
 
 int main(void)
 
 {
+        (51 chars long)
 
+	[stderr]: 
+
+	(0 chars long)
+
+	[Expected]
 	puts("Programming is like building a multilingual puzzle");
 
 	return (0);
