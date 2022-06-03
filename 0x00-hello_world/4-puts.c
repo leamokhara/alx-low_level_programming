ff494a4aaa5e7ed3773f28a06f2e2251488a3d4f
leamokhara <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@github.com>
2022-06-03 11:16:25 -0700

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,14 +4,12 @@
  *
  * * Return: 0 if successful
  *
- * 
+ *
  **/
 
 int main(void)
 
-{
-       	puts("Programming is like building a multilingual puzzle");
-
+{	puts("Programming is like building a multilingual puzzle");
 	return (0);
 
 }
