261d43d6007f0d4dbe866c79c03c875c1ef96f0b
leamokhara <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@github.com>
2022-06-04 19:28:00 -0700

    positive -negetive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Prints a random number and states whether
- * 	it is positive, negative, or zero.
+ *	it is positive, negative, or zero.
  *
  * Return: Always 0.
  */
@@ -21,6 +21,7 @@ int main(void)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
-	
+
+
 	return (0);
 }
