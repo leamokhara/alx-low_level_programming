8a560da7c8a0418ce211cca10d83f596a319a6e9
leamokhara <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@github.com>
2022-06-05 14:00:21 -0700

    5 prints

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * my_first c - programming
- * main - entry point
- *
- * Return: 0 if sucessful
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
+
