ce1538f57ea6832fc4527530311acb25bd44b312
leamokhara <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'github.com>
2022-06-08 19:34:06 -0700

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  * print_times_table - function that prints the n times table, starting with 0.
  * Description: If `n` is greater than 15 or less than 0, print nothing.
  *
