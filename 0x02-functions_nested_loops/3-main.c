6962aed1d6646fbb5ab05dc02251ef93c85a1021
leamokhara <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@github.com>
2022-06-08 17:04:41 -0700

    three is lower

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase letters
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
