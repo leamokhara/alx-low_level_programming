07623bafb1cd05ecb58a675b22f5dbde317d177a
leamokhara <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@github.com>
2022-06-15 08:59:54 -0700

    encodes a string

--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -5,6 +5,13 @@
  *
  * Return: Always 0
  */
-char *string_toupper(char *str)
+int main(void)
 {
+	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+	char *p;
 
+	p = leet(s);
+	printf("%s", p);
+	printf("%s", s);
+	return (0);
+}
