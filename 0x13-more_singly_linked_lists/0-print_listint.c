40819f60682acec899c7c6db628c450e7e993c75
leamokhara <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@github.com>
2022-07-18 08:22:48 -0700

    hit fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	int counter = 0;
-	while (h != '\0')
+	size_t count = 0;
+
+	while (h != NULL)
 	{
-		printf("%d/n", h->n)
+		printf("%d/n", h->n);
 			h = h->next;
-		counter++;
+		count++;
 	}
-	return (counter);
+	return (count);
 }
