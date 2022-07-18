58cedbbcdd3a0cc9c345549ae056a8e120675024
leamokhara <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@github.com>
2022-07-18 10:18:51 -0700

    hot fix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,10 +14,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	while (head)
 	{
-		 if (i == index)
-		 return (head);
-		 head = head->next;
-		 ++i;
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
 	}
 	return (NULL);
 }
