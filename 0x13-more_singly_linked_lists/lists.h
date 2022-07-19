007e5a72da791173f0cee45c2e0a7ba437604395
leamokhara <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@github.com>
2022-07-19 05:44:48 -0700

    hot fix

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -33,4 +33,5 @@ listint_t *reverse_recur(listint_t *first, listint_t *second);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 #endif
