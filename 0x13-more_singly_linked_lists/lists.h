0d160c79e3d577a085c18a244f250e96e55fb6be
leamokhara <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@github.com>
2022-07-19 04:47:23 -0700

    Write a function that prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -33,5 +33,5 @@ listint_t *reverse_recur(listint_t *first, listint_t *second);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-
+listint_t *find_listint_loop_pl(listint_t *head)
 #endif
